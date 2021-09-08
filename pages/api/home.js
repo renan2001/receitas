7fdf188bb74ce0e2e3d2777b781dd0e3fe6cc455
renan2001 <renancastro2216@gmail.com>
2021-09-08 14:21:46 -0300

    first

--- pages/api/home.js
+++ pages/api/home.js
@@ -1,6 +1,6 @@
-import Menu from '../components/menu'
-import Footer from '../components/footer'
-import Receitas from '../components/receitas'
+import Menu from '../../components/menu'
+import Footer from '../../components/footer'
+import Receitas from '../../components/receitas'
 
 function Home(){
     return (
