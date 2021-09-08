21943ca9f24df6200778f4b1ec94b345e2e7401a
renan2001 <renancastro2216@gmail.com>
2021-09-08 14:11:52 -0300

    first

--- components/receitas.js
+++ components/receitas.js
@@ -1,7 +1,3 @@
-import Menu from "./menu"
-import Image from 'next/image'
-import Link  from 'next/link'
-import Foto from '../imagens/hamburguer.jpg'
 import styles from '../styles/Home.module.css'
 
 function Receitas(){
