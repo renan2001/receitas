ad419f99e0a6bb2c21d18f4671c90a4cd1076f0e
renan2001 <renancastro2216@gmail.com>
2021-09-10 17:03:27 -0300

    update

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,7 @@ function Footer(){
     return (
         <>
             <footer className={styles.footer}>
-                <Link      href="https://pt-br.facebook.com/"
+                <Link  href="https://pt-br.facebook.com/"
                 target="_blank"
                 rel="noopener noreferrer">
                
