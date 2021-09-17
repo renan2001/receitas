057c49af76de69f25a61618f9b2d6481fa72f89f
renan2001 <renancastro2216@gmail.com>
2021-09-17 14:35:40 -0300

    comit

--- pages/pimentao.js
+++ pages/pimentao.js
@@ -40,7 +40,7 @@ function Pimentao(){
 <p>
 <ul>
     <li>1 pimentão grande</li>
-    <li>150 g de queijo cottage - cerca de uma xícara</li>
+    <li>150 g de queijo cottage cerca de uma xícara</li>
     <li>2 colheres de sopa de molho de tomate</li>
     <li>2 fatias de muçarela</li>
     <li>Ervas à gosto</li >
@@ -54,7 +54,7 @@ function Pimentao(){
 
 <ul>
     <li>Pré-aqueça o forno a 200ºC</li>
-    <li>Limpe o pimentão, corte o top, remova o interior e faça uma "base" de queijo muçarela</li>
+    <li>Limpe o pimentão, corte o top, remova o interior e faça uma base de queijo muçarela</li>
     <li>Em uma tigela, misture o queijo cottage, o molho de tomate, as ervas (eu usei manjericão e salsinha), sal e pimenta do reino à gosto.</li>
     <li>Depois, coloque o recheio no pimentão e cubra com uma fatia de queijo muçarela. Leve ao forno por 20 minutos</li>
     Bom apetite!
